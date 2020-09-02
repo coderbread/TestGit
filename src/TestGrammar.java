05a2bfd35e1f23143e486ae0def23f8d0c9196b7
coderbread <1159389354@qq.com>
2020-09-02 22:26:21 +0800

    初始化代码

---
+++ src/TestGrammar.java
@@ -0,0 +1,14 @@
+public class TestGrammar {
+    public static void main(String[] args) {
+        //通过对象，调用普通对象
+        TestGrammar tg = new TestGrammar();
+        tg.printInfo();
+        System.out.println(tg.add(1,2) + 10);
+    }
+    void printInfo(){
+        System.out.println("我的天");
+    }
+    int add(int a, int b){
+        return a + b;
+    }
+}
